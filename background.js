9f69b448a53e45655541820ab3fe540df972c5bb
AsinChen <shinningstar1001@gmail.com>
2015-03-12 19:28:49 +0800

    Change the way to inject script

--- background.js
+++ background.js
@@ -1,33 +1,18 @@
 /**
  * Created by Andy on 2015/3/12.
  */
-var defaultIcon = "mouse_blue.png";
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.browserAction.setIcon({
-        path: changeIcon()
-        //tabId: sender.tab.id
+        path: "mouse_red.png",
+        tabId: tab.id
     });
-
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {click:true}, function(response) {
-
-        });
-    });
+    chrome.tabs.executeScript(null, {file: "unlock.js"});      
 });
 
-function changeIcon(){
-    return defaultIcon = defaultIcon == "mouse_blue.png" ? "mouse_red.png" : "mouse_blue.png";
-}
+chrome.commands.onCommand.addListener(function(command)  //for rapid reload
+{ 
+  if (command == 'reload_extension')
+    chrome.runtime.reload();
+});
 
-
-
-//
-//function defaultHandler(event) {
-//    event.returnValue = true;
-//}
-//for (event_type in ['selectstart', 'copy', 'cut', 'paste', 'keydown', 'contextmenu', 'dragstart']) {
-//    document.documentElement.addEventListener(event_type, defaultHandler);
-//    document.body.addEventListener(event_type, defaultHandler);
-//    document.addEventListener(event_type, defaultHandler);
-//}
